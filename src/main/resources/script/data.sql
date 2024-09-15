9bf14ef09953b53608a203b6144096950fa0bb6f
felipe <f.marinho162001@gmail.com>
2024-09-14 22:18:05 -0300

    feat:  sql para inserção do admin

--- src/main/resources/script/data.sql
+++ src/main/resources/script/data.sql
@@ -14,3 +14,8 @@ INSERT  INTO perfil_acesso_permissoes (perfil_acesso_id, permissao_id) VALUES (1
 INSERT  INTO perfil_acesso_permissoes (perfil_acesso_id, permissao_id) VALUES (1, 3);
 INSERT  INTO perfil_acesso_permissoes (perfil_acesso_id, permissao_id) VALUES (1, 4);
 INSERT  INTO perfil_acesso_permissoes  (perfil_acesso_id, permissao_id) VALUES (2, 4);
+
+-- ADMIN
+INSERT INTO usuario (username, password, perfil_acesso_id, primeiro_acesso) VALUES  ('admin', '$2a$10$dv8Hg/2GJ7hiBLfA5iEE2uThd66wlZbO1YQM6eaj0GAKQDxMwGwqq', 1, false);
+INSERT INTO persona (dtype,  nome, sobrenome, data_nascimento, sexo, email, ddd, numero_telefone,  inativo)
+    VALUES  ('Colaborador', 'admin', 'admin', '2021-01-01', 'MASCULINO', 'admin@gmail.com', '69', '99999999', false);
