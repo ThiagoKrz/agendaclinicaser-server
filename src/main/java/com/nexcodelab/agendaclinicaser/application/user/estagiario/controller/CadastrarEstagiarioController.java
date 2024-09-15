5cfd67dbf8b3948f39641cf9cf3970a262269161
felipe <f.marinho162001@gmail.com>
2024-09-14 22:21:43 -0300

    feat: comentados endpoint nao utilizado

--- src/main/java/com/nexcodelab/agendaclinicaser/application/user/estagiario/controller/CadastrarEstagiarioController.java
+++ src/main/java/com/nexcodelab/agendaclinicaser/application/user/estagiario/controller/CadastrarEstagiarioController.java
@@ -1,31 +1,31 @@
-package com.nexcodelab.agendaclinicaser.application.user.estagiario.controller;
-
-import com.nexcodelab.agendaclinicaser.application.user.colaborador.dto.request.CadastrarColaboradorRequest;
-import com.nexcodelab.agendaclinicaser.application.user.colaborador.model.Colaborador;
-import com.nexcodelab.agendaclinicaser.application.user.estagiario.dto.request.CadastrarEstagiarioRequest;
-import com.nexcodelab.agendaclinicaser.application.user.estagiario.model.Estagiario;
-import com.nexcodelab.agendaclinicaser.application.user.persona.service.CadastrarPersonaUseCase;
-import com.nexcodelab.agendaclinicaser.application.user.usuario.model.Usuario;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping
-@RequiredArgsConstructor
-public class CadastrarEstagiarioController {
-
-    private final CadastrarPersonaUseCase<Estagiario> service;
-
-    @PostMapping("/estagiario")
-    public ResponseEntity<Void> execute(@Valid @RequestBody CadastrarEstagiarioRequest request) {
-        service.execute(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-}
+//package com.nexcodelab.agendaclinicaser.application.user.estagiario.controller;
+//
+//import com.nexcodelab.agendaclinicaser.application.user.colaborador.dto.request.CadastrarColaboradorRequest;
+//import com.nexcodelab.agendaclinicaser.application.user.colaborador.model.Colaborador;
+//import com.nexcodelab.agendaclinicaser.application.user.estagiario.dto.request.CadastrarEstagiarioRequest;
+//import com.nexcodelab.agendaclinicaser.application.user.estagiario.model.Estagiario;
+//import com.nexcodelab.agendaclinicaser.application.user.persona.service.CadastrarPersonaUseCase;
+//import com.nexcodelab.agendaclinicaser.application.user.usuario.model.Usuario;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping
+//@RequiredArgsConstructor
+//public class CadastrarEstagiarioController {
+//
+//    private final CadastrarPersonaUseCase<Estagiario> service;
+//
+//    @PostMapping("/estagiario")
+//    public ResponseEntity<Void> execute(@Valid @RequestBody CadastrarEstagiarioRequest request) {
+//        service.execute(request);
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
+//}
--- src/main/java/com/nexcodelab/agendaclinicaser/application/user/paciente/controller/command/CadastrarPacienteController.java
+++ src/main/java/com/nexcodelab/agendaclinicaser/application/user/paciente/controller/command/CadastrarPacienteController.java
@@ -1,29 +1,29 @@
-package com.nexcodelab.agendaclinicaser.application.user.paciente.controller.command;
-
-import com.nexcodelab.agendaclinicaser.application.user.paciente.dto.request.CadastrarPacienteRequest;
-import com.nexcodelab.agendaclinicaser.application.user.paciente.model.Paciente;
-import com.nexcodelab.agendaclinicaser.application.user.persona.service.CadastrarPersonaUseCase;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping
-@RequiredArgsConstructor
-public class CadastrarPacienteController {
-
-    private final CadastrarPersonaUseCase<Paciente> service;
-
-    @PostMapping("/paciente")
-    public ResponseEntity<Void> execute(@Valid @RequestBody CadastrarPacienteRequest request) {
-        service.execute(request);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-}
+//package com.nexcodelab.agendaclinicaser.application.user.paciente.controller.command;
+//
+//import com.nexcodelab.agendaclinicaser.application.user.paciente.dto.request.CadastrarPacienteRequest;
+//import com.nexcodelab.agendaclinicaser.application.user.paciente.model.Paciente;
+//import com.nexcodelab.agendaclinicaser.application.user.persona.service.CadastrarPersonaUseCase;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping
+//@RequiredArgsConstructor
+//public class CadastrarPacienteController {
+//
+//    private final CadastrarPersonaUseCase<Paciente> service;
+//
+//    @PostMapping("/paciente")
+//    public ResponseEntity<Void> execute(@Valid @RequestBody CadastrarPacienteRequest request) {
+//        service.execute(request);
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).build();
+//    }
+//
+//}
